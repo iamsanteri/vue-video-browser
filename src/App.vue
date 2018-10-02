54ee4c7a59f5d32c4e78568d1ba5c84193f73319
Santeri Liukkonen <sliukkonen@Santeris-MacBook-Pro.local>
2018-10-02 22:45:32 +0200

    Added an event emitter from child to parent and a prop passdown to a child component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <SearchBar @termChange="onTermChange"></SearchBar>
-        <VideoList></VideoList>
+        <VideoList :videos="videos"></VideoList>
     </div>
 </template>
 
@@ -11,7 +11,7 @@
     // 1. Add component import with relative path in <script> tags
     // 2. Add component to the template of the parent
     // 3. Add component to the components object of export default
-    
+
     import axios from "axios";
     import SearchBar from "./components/SearchBar";
     import VideoList from "./components/VideoList";
@@ -24,6 +24,12 @@
             SearchBar,
             VideoList
         },
+        // If in components (not in Vue instance) define data as a function which returns the object
+        data() {
+            return {
+                videos: []
+            };
+        },
         methods: {
             onTermChange(searchTerm) {
                 axios.get("https://www.googleapis.com/youtube/v3/search", {
@@ -33,7 +39,9 @@
                         part: "snippet",
                         q: searchTerm
                     }
-                }).then(response => console.log(response));
+                }).then(response => {
+                    this.videos = response.data.items;
+                });
             }
         }
     };
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,17 @@
 <template>
     <ul>
         VideoList
+        {{ videos.length }}
     </ul>
 </template>
 
 <script>
     export default {
-        name: "VideoList"
+        name: "VideoList",
+        // Accept props as an array or as an object with slight type validation
+        props: {
+            videos: Array
+        }
     };
 </script>
 
