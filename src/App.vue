010e83c4d1cb5ae2a474c64b13bf13367a181cf3
Santeri Liukkonen <sliukkonen@Santeris-MacBook-Pro.local>
2018-10-02 21:41:37 +0200

    Re-started src folder from scratch. Started building search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar></SearchBar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // Remember to import components to the housing component
+    import SearchBar from "./components/SearchBar";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+        // Let Vue know that you will have component within the template!
+        components: {
+            SearchBar
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
